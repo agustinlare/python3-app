dcd7b3b7411b5de338e57a995bc43e0081b6201b
Agustin Lavarello <agustinlare@gmail.com>
2023-05-12 20:47:24 -0300

    added some other endpoints and a better readme

--- README.md
+++ README.md
@@ -2,26 +2,35 @@
 
 Flask app that returns a json with the aws identity caller.
 
-## Example response
+
+
+## Prerequisites
+
+- Python (version 3.6 or higher) installed
+- AWS credentials properly configured if using the `/` endpoint
+
+## Endpoints
+
+The application provides the following endpoints:
+
+- `/`: Returns the AWS caller identity information in JSON format.
+- `/counter`: Returns the current count of requests made to the server.
+- `/ping`: Returns "pong" as a response.
+- `/switch`: Toggles the switch status between true and false, returning the updated status as a JSON response.
+- `/liveness`: Returns a response indicating the liveness probe status (200 OK if the switch is true, otherwise 503 Service Unavailable).
+- `/readiness`: Returns a response indicating the readiness probe status (200 OK if the switch is true, otherwise 503 Service Unavailable).
+
+### Example response
 
 - `/`
 ```=json
 {
-  "identity_caller": {
-    "Account": "111111111",
-    "Arn": "arn:aws:iam::111111111:user/foo.var",
-    "ResponseMetadata": {
-      "HTTPHeaders": {
-        "content-length": "409",
-        "content-type": "text/xml",
-        "date": "Fri, 12 May 2023 22:22:26 GMT",
-        "x-amzn-requestid": "234v23sdfg-1d14-442b-95e5-adec67742dab"
-      },
-      "HTTPStatusCode": 200,
-      "RequestId": "234v23sdfg-1d14-442b-95e5-adec67742dab",
-      "RetryAttempts": 0
-    },
-    "UserId": "C4NTH1SB3AR34LUS3R1D"
-  }
+  "Account": "340604397924",
+  "Arn": "arn:aws:iam::340604397924:user/agustin.lare",
+  "UserId": "AIDAU6TM2VFSMWUNJZN5W"
 }
 ```
+
+## Configuration
+
+No additional configuration is required to run the application. However, if you want to use the / endpoint to retrieve AWS caller identity information, ensure that your AWS credentials are properly configured.
--- app.py
+++ app.py
@@ -1,15 +1,76 @@
-from flask import Flask
-import boto3
+from flask import Flask, jsonify
+from flask import request
+from boto3.session import Session
+import logging
+import threading
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-  s = boto3.Session()
-  c = s.client("sts")
-  
-  asd = c.get_caller_identity()
-  return { "identity_caller": asd }
+counter = 0
+counter_lock = threading.Lock()
+switch_status = True
+switch_lock = threading.Lock()
+
+@app.route("/")
+def get_caller_identity():
+    session = Session()
+    sts_client = session.client('sts')
+
+    try:
+        response = sts_client.get_caller_identity()
+        caller_identity = {
+            'Account': response['Account'],
+            'Arn': response['Arn'],
+            'UserId': response['UserId']
+        }
+        return jsonify(caller_identity), 200
+    except Exception as e:
+        logging.error("Error retrieving caller identity: %s", str(e))
+        return jsonify({'error': 'Failed to retrieve caller identity'}), 500
+
+@app.route("/counter")
+def get_counter():
+    response = {'count': counter}
+    return jsonify(response), 200
+
+@app.route("/ping")
+def ping():
+    return 'pong', 200
+
+@app.route("/switch", methods=['GET', 'POST'])
+def toggle_switch():
+    global switch_status
+    switch_lock.acquire()
+    switch_status = not switch_status
+    switch_lock.release()
+
+    response = {'status': switch_status}
+    return jsonify(response), 200
+
+@app.route("/liveness")
+def liveness():
+    global switch_status
+    if switch_status:
+        return 'Liveness Probe', 200
+    else:
+        return 'Liveness Probe', 503
+
+@app.route("/readiness")
+def readiness():
+    global switch_status
+    if switch_status:
+        return 'Readiness Probe', 200
+    else:
+        return 'Readiness Probe', 503
+
+def increment_counter():
+    global counter
+    with counter_lock:
+        counter += 1
+
+@app.before_request
+def before_request():
+    increment_counter()
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0",port=8080)
